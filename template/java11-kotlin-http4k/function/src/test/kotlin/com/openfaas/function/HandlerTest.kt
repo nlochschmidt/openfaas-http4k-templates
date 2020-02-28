df9052442561beb5d69dba952cd42e1969792d0e
Niklas Lochschmidt <nlochschmidt@gmail.com>
2020-02-29 00:50:50 +0100

    Include hamkrest testing by default

--- template/java11-kotlin-http4k/function/src/test/kotlin/com/openfaas/function/HandlerTest.kt
+++ template/java11-kotlin-http4k/function/src/test/kotlin/com/openfaas/function/HandlerTest.kt
@@ -1,9 +1,12 @@
 package com.openfaas.function
 
+import com.natpryce.hamkrest.assertion.assertThat
 import org.http4k.core.Method.GET
 import org.http4k.core.Method.POST
 import org.http4k.core.Request
-import org.junit.jupiter.api.Assertions.*
+import org.http4k.core.Status.Companion.OK
+import org.http4k.hamkrest.hasBody
+import org.http4k.hamkrest.hasStatus
 import org.junit.jupiter.api.Test
 
 internal class HandlerTest {
@@ -13,14 +16,14 @@ internal class HandlerTest {
     @Test
     fun `on GET should respond with 200 and message`() {
         val response = handler(Request(GET, "/"))
-        assertEquals(200, response.status.code)
-        assertEquals("Hello, world!", response.bodyString())
+        assertThat(response, hasStatus(OK))
+        assertThat(response, hasBody("Hello, world!"))
     }
 
     @Test
     fun `on POST should respond with 200 and message`() {
         val response = handler(Request(POST, "/"))
-        assertEquals(200, response.status.code)
-        assertEquals("Hello, world!", response.bodyString())
+        assertThat(response, hasStatus(OK))
+        assertThat(response, hasBody("Hello, world!"))
     }
 }
